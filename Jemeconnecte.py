ca79ad10c45baba2f6f1892c2b3b2f9057457243
Colin Goutte <cgte@bk.ru>
2018-03-03 14:20:59 +0100

    First shot ofworking example with download

---
+++ Jemeconnecte.py
@@ -0,0 +1,114 @@
+# -*- coding: utf-8 -*-
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select
+from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoAlertPresentException
+import unittest, time, re
+
+from credentials import emailaddress, password
+from bs4 import BeautifulSoup as BSoup
+
+def inference_balises(tag):
+    grandparent = tag.parent.parent
+    parent_class = [u'wpb_text_column', u'wpb_content_element', u'']
+    cond = (
+        tag.has_attr('class') and tag['class'] == [u'wpb_wrapper']
+        and
+        tag.parent.has_attr('class')
+            and tag.parent['class'] == parent_class
+        and grandparent.has_attr('class')
+            and grandparent['class'] == [u'wpb_wrapper']
+            )
+    if not cond: return False
+
+    links =  tag.find_all('a')
+    return len(links) == 2
+
+def urls_title_from_tag(tag):
+    atags = tag.find_all('a')
+    refs = [a['href'] for a in atags]
+    #get title
+    title = None
+    for atag in atags:
+        if atag['href'].lower().endswith('mp3'):
+            title = atag.text
+            print 'Parsing ok', title
+            break
+    else:
+        print 'Parse issue for'
+        print tag.prettify()
+    return refs, title
+
+class JeMeConnecte(unittest.TestCase):
+    def setUp(self):
+        self.driver = webdriver.Firefox()
+        self.driver.implicitly_wait(30)
+        self.base_url = "https://russianpodcast.eu/"
+        self.verificationErrors = []
+        self.accept_next_alert = False
+
+    def test_je_me_connecte(self):
+        driver = self.driver
+        driver.get("https://russianpodcast.eu/russian-dacha-club/")
+        driver.find_element_by_name("email").click()
+        driver.find_element_by_name("email").clear()
+        driver.find_element_by_name("email").send_keys(emailaddress)
+        driver.find_element_by_name("password").click()
+        driver.find_element_by_name("password").clear()
+        driver.find_element_by_name("password").send_keys(password)
+        driver.find_element_by_name("LoginDAPLoginForm").click()
+        from time import sleep
+        sleep(2)
+        driver.refresh() # The url is the same so you need to re-fetch the content
+        klass = 'wpb_text_column wpb_content_element '
+
+        soup = BSoup(driver.page_source, 'html.parser')
+        megatop =  soup.find_all(inference_balises)
+        import ipdb
+        print(len(megatop))
+        to_fetch = []
+        for m in megatop:
+            res = urls_title_from_tag(m)
+            to_fetch.append(res)
+        import urllib2
+        for links, title in to_fetch:
+            for link in links:
+                filename = link.split('/')[-1]
+
+                with open(filename, 'wb') as f:
+                    f.write(urllib2.urlopen(link).read())
+            print title
+            print links
+            print '---'
+        ipdb.set_trace()
+
+
+    def is_element_present(self, how, what):
+        try: self.driver.find_element(by=how, value=what)
+        except NoSuchElementException as e: return False
+        return True
+
+    def is_alert_present(self):
+        try: self.driver.switch_to_alert()
+        except NoAlertPresentException as e: return False
+        return True
+
+    def close_alert_and_get_its_text(self):
+        try:
+            alert = self.driver.switch_to_alert()
+            alert_text = alert.text
+            if self.accept_next_alert:
+                alert.accept()
+            else:
+                alert.dismiss()
+            return alert_text
+        finally: self.accept_next_alert = True
+
+    def tearDown(self):
+        self.driver.quit()
+        self.assertEqual([], self.verificationErrors)
+
+if __name__ == "__main__":
+    unittest.main()
