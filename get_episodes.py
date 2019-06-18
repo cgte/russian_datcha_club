37aa3422f7f4a80e07193f284c1bb15cb945dd3f
Colin Goutte <cgte@bk.ru>
2019-06-18 11:55:07 +0200

    Refactor works

--- get_episodes.py
+++ get_episodes.py
@@ -1,4 +1,9 @@
 # -*- coding: utf-8 -*-
+from time import sleep
+import urllib2
+import os
+from pprint import pprint
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
@@ -50,68 +55,86 @@ print_pdfs_with_default_printer = False # Uses default printer on linux
 target_subfolder = 'klimova'
 quiet = True
 
+
+
+
+
 class PodcastGetter(object):
     def __init__(self, driver):
         self.driver = driver
+        self.driver.implicitly_wait(20)
 
-    def __call__(self):
-        self.setUp()
-        self.test_je_me_connecte()
-        self.tearDown()
+        if not os.path.exists(target_subfolder):
+            os.makedirs(target_subfolder)
+        os.chdir(target_subfolder)  # Use proper context manager instead
+        self.present_files = [f for f in os.listdir('.')  if os.path.isfile(f)]
+        print 'init done'
 
-
-    def setUp(self):
-        self.driver.implicitly_wait(30)
-        self.base_url = "https://russianpodcast.eu"
-        self.accept_next_alert = False
-
-    def test_je_me_connecte(self):
+    def log_in(self):
+        print 'logging in ',
+        import sys; sys.stdout.flush()
         driver = self.driver
         driver.get("https://russianpodcast.eu/russian-dacha-club")
         driver.find_element_by_name("email").send_keys(emailaddress)
         driver.find_element_by_name("password").send_keys(password)
         driver.find_element_by_name("LoginDAPLoginForm").click()
-        from time import sleep
-        sleep(2)
+        print 'logged in'
+
+    def refreshed_page_source(self, delay=2):
+        """ This is a heck because getting the content right after login
+        gives you le login page content """
+        driver = self.driver
+
+        sleep(delay)
         driver.refresh() # The url is the same so you need to re-fetch the content
-        klass = 'wpb_text_column wpb_content_element '
+        print 'refresed'
+        sleep(0.5)
+        source = driver.page_source
+        return source
 
-        soup = BSoup(driver.page_source, 'html.parser')
-        megatop =  soup.find_all(inference_balises)
+    def __call__(self):
+        self.log_in()
+        page_source = self.refreshed_page_source()
+        print 'got source'
         self.driver.quit()
-        print(len(megatop))
-        to_fetch = []
-        for m in megatop:
-            res = urls_title_from_tag(m)
-            to_fetch.append(res)
-        import urllib2
-        import os
-        if not os.path.exists(target_subfolder):
-            os.makedirs(target_subfolder)
-        os.chdir(target_subfolder)
-        present_files = [f for f in os.listdir('.')
-                if os.path.isfile(f)]
-        from pprint import pprint
+        print 'quitted driver'
+        links_filenames = self.get_links_and_titles(
+            source=page_source,
+            finder_function=inference_balises
+            )
+        files_to_get = self.filter_files(links_filenames)
+        self.process_files(files_to_get)
 
-        downloaded = []
-        for links, title in to_fetch:
-            for link in links:
-                filename = link.split('/')[-1]
-                if filename not in present_files:
-                    with open(filename, 'wb') as f:
-                        f.write(urllib2.urlopen(link).read())
-                        downloaded.append(title)
-                    if filename.lower().endswith('pdf'):
-                        if print_pdfs_with_default_printer:
-                            print(os.system('lp %s' % filename))
-                else:
-                    print(filename + ' already here')
-        print('Downloaded files')
-        for title in downloaded:
-            print(title)
 
-    def tearDown(self):
-        self.driver.quit()
+
+    @staticmethod
+    def get_links_and_titles(source, finder_function):
+        soup = BSoup(source, 'html.parser')
+        items = soup.find_all(finder_function)
+        return [urls_title_from_tag(m) for m in items]
+
+
+    def filter_files(self, links_and_title):
+        def filename(link):
+            return link.split('/')[-1]
+        def is_there(link_title):
+            return filename(link_title) in self.present_files
+
+        return [(link, filename(link), title)
+                for links, title in links_and_title
+                for link in links
+                if not is_there(link)]
+
+
+    def process_files(self, files_to_get):
+        for link, filename, title in files_to_get:
+            print 'processing:\n', '\n'.join([link, filename, title])
+            with open(filename, 'wb') as f:
+                f.write(urllib2.urlopen(link).read())
+                if filename.lower().endswith('pdf') and \
+                        print_pdfs_with_default_printer:
+                    print(os.system('lp %s' % filename))
+
 
 if __name__ == "__main__":
     try:
